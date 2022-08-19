ea50465c55305760d5a1ddda2ecd151e6bf17a81
LAPTOP-QQ2PAQES\welki <welkiv.shaker@gmail.com>
2022-08-19 17:29:48 +0200

    React form

--- web/simulator/static/simulator/scripts/form-elements.js
+++ web/simulator/static/simulator/scripts/form-elements.js
@@ -4,43 +4,128 @@ class Form extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            matrix: [],
-            base_elements: [],
-            composition: [],
-            specification: []
-        }   
+            forms: {},
+            models: {}
+        }
     }
 
-    async componentDidMount(){
-        const base_elements = await fetch('/api/base_element').then(result => result.json())
+    componentDidMount(){
+        let forms = {}, models = {}
+        const types = ['base_element', 'base_element_value', 'specification', 'possible_specification', 'composition']
+        Promise.all(types.map(element => {
+            return new Promise(async(resolve) => {
+                const model = await fetch('/api/base_element', {
+                    method: 'OPTIONS'
+                }).then(response => response.json())
+                models[element] = model.actions.POST
+                forms[element] =  await fetch(`/api/${element}`).then(response => response.json())
+                if(models[element] && forms[element]) resolve()
+            })
+        })).then(() => {
+            console.log(forms, models)
+            this.setState({
+                forms: { forms },
+                models: { models }
+            }, () => {
+                console.log(this.state)
+            })
+        })
+        // types.map(async(element) => {
+        //     const model = await fetch('/api/base_element', {
+        //         method: 'OPTIONS'
+        //     }).then(response => response.json())
+        //     models[element] = model.actions.POST
+        //     forms[element] =  await fetch(`/api/${element}`).then(response => response.json())
+        // })
+        console.log(this.state.forms, this.state.models)
+        
+        
+        
     }
 
     render(){
         return(
-            <section className="form-wrp">
-                <div>
-                    <button className="button" onClick={() => addMatrix()}>
-                        <i>+</i>
-                        <span>Add Matrix</span>   
-                    </button>
-                </div>
-                <section>
-                    <div>
-                    {
-                        this.state.matrix.map(item => {
-                            return <Matrix datas={item}/>
-                        })
-                    }
-                </div>
+            this.state.forms && this.state.models ?
+            (
+                <section className="form-elements-wrp">
+                    <Elements type='base_element' forms={this.state.forms['base_element']} model={this.state.models['base_element']}/>
+                    <Elements type='possible_specification' forms={this.state.forms.possible_specification} model={this.state.models.possible_specification} />
+                    <Elements type='composition' forms={this.state.forms.base_element_value} model={this.state.models.base_element_value}>
+                        <Elements type='specification' forms={this.state.forms.specification} model={this.state.models.specification} />
+                    </Elements>
                 </section>
-            </section>
+            ) : <div></div>
         )
     }
 }
 
-function Matrix({datas}){
+function Elements({ type, forms, model, count, children }){
+
+
+    React.useEffect(() => {
+        console.log('reload', forms, model)
+    }, [forms, count])
+
     return(
-        <section>
+        <ul>
+            <section className="button-wrp">
+                <button>Add { type.replaceAll('_', ' ') }</button>
+            </section>
+            {
+                forms && forms.map(form => {
+                    return(
+                        <li key={form.id}>
+                            <Element model={model} form={form} />
+                        </li>
+                    )
+                })
+            }
+            { children }
+        </ul>
+    )
+}
+
+function Element({ model, form }){
+    
+    const types = {
+        'field': 'text',
+        'string': 'text',
+        'float': 'number'
+    }
+
+    const renderField = (key, field, value) => {
+        return(
+            <div class="field-wrp">
+                <label>{ field.label }</label>
+                { renderInput(key, field, value) }
+            </div>
+        )
+    }
+
+    const renderInput = (key, field, value) => {
+        if(model[key].type === 'choice'){
+            return(
+                <select name={key}>
+                    {
+                        model[key].choices.map(option => {
+                            return <option value={option.value}>{option.display_name}</option>
+                        })
+                    }
+                </select>
+            )
+        }
+        return(
+            <input name={key} type={types[field.type]} required={field.required} value={value} />
+        )
+    }
+
+    return(
+        <section className="form-wrp">
+        {
+            model && [...model].map(key => {
+                if(!model[key].read_only) return renderField(key, model[key], form && form[key] || null)
+            })
+        }
         </section>
     )
 }
